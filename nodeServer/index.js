bfd10ae897b0f971b22007da78ab74af83819711
Preeti <preetiyadav5443@gmail.com>
2024-12-10 10:43:46 +0530

    Serve frontend with Express and integrate Socket.io

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,5 +1,11 @@
-// node server which will handle socket io connection
-const io = require("socket.io")(8000, {
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
@@ -8,18 +14,37 @@ const io = require("socket.io")(8000, {
 
 const users = {};
 
+// Serve static files (HTML, CSS, JS) from the root directory
+app.use(express.static(path.join(__dirname, '../')));
+
+// Route to serve the main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// Handle Socket.io connections
 io.on('connection', socket => {
+    console.log('A user connected');
 
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name)
-    })
+        socket.broadcast.emit('user-joined', name);
+    });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
-    })
-    socket.on('disconnect', message => {
-        socket.broadcast.emit('left', users[socket.id])
-        delete users[socket.id];
-    })
-})  
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            socket.broadcast.emit('left', users[socket.id]);
+            delete users[socket.id];
+        }
+    });
+});
+
+// Start the server on the specified port
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
